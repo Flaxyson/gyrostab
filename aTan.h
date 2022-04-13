01006735c7e2212065146892c58ddaffd88dcad9
Flaxyson <max.mg.gulda@gmail.com>
2022-04-13 13:28:38 +0200

    framework

--- aTan.h
+++
@@ -1,3 +0,0 @@
-#include "gd32vf103.h"
-
-int arctan(int x, int y);
--- servo.h
+++
@@ -1,9 +0,0 @@
-#include "gd32vf103.h"
-
-void initServoA(void);
-
-void u0init(int enable);
-
-void USART0_init();
-
-void moveServo(int degrees);
