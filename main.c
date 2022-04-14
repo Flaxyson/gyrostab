e9093a39272dd006edf4ad30ebb2673b9d33ae86
Flaxyson <max.mg.gulda@gmail.com>
2022-04-14 20:56:56 +0200

    clean up

--- main.c
+++ main.c
@@ -6,8 +6,6 @@
 #include "aTan.h"
 
 
-void motorStartupSeq(int endThrottle);
-
 int main(void){
     InitPWM();
     initServoA();
@@ -22,35 +20,16 @@ int main(void){
        ICM-20600 is mostly register compatible with MPU6500, if MPU6500 is used only thing that needs 
        to change is MPU6500_WHO_AM_I_ID from 0x11 to 0x70. */
     mpu6500_install(I2C0);
-    //motorStartupSeq(0);
+    motorStartupSeq(0);
     
     while(1){
-        
-        //for(int i = 0; i < 2 ; i++){
-        //    mpu6500_getAccel(&vecA);
-        //    vecG.x+=vecA.x;
-        //    vecG.y+=vecA.y;
-        //    vecG.z+=vecA.z;
-        //}
-        //vecA.x = ((int)vecG.x>>1);
-        //vecA.y = ((int)vecG.y>>1);
-        //vecA.z = ((int)vecG.z>>1);
 
         mpu6500_getAccel(&vecA);
         MoveServoA(arctan(vecA.z , (int)vecA.x));
         MoveServoB(arctan(vecA.z , -(int)vecA.y));
-        //timer_channel_output_pulse_value_config(TIMER0, TIMER_CH_0, 550);
+
     }
 }
 
-void motorStartupSeq(int endThrottle){
-    SetMotorA(2000);
-    SetMotorB(2000);
-    delay_1ms(2000);
-    SetMotorA(1000);
-    SetMotorB(1000);
-    delay_1ms(2000);
-    SetMotorA(endThrottle);
-    SetMotorB(endThrottle);
-}
+
 
